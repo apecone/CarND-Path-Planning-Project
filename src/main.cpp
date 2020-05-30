46b2194c884df6b5338e7eb9cff53b176fba77fe
Anthony Pecone <apecone@gmail.com>
2020-05-30 10:16:51 -0400

    Using QA session, car smoothly transitions between paths.

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 #include "helpers.h"
 #include "json.hpp"
 #include "spline.h"
+//#include "Behavior.h"
 
 // for convenience
 using nlohmann::json;
@@ -50,6 +51,8 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
+    
+  //Vehicle vehicle = Vehicle();
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -77,6 +80,8 @@ int main() {
           double car_d = j[1]["d"];
           double car_yaw = j[1]["yaw"];
           double car_speed = j[1]["speed"];
+            
+          double ref_vel = 49.5;
 
           // Previous path data given to the Planner
           auto previous_path_x = j[1]["previous_path_x"];
@@ -89,9 +94,71 @@ int main() {
           // Sensor Fusion Data, a list of all other cars on the same side 
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
+            
+          int prev_size = previous_path_x.size();
+            
+          vector<double> ptsx;
+          vector<double> ptsy;
+            
+          // reference x, y, yaw states
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+            
+          if (prev_size < 2) {
+              // Use two points that make the path tangent to the car
+              ptsx.push_back(car_x - cos(car_yaw));
+              ptsx.push_back(car_x);
+              
+              ptsy.push_back(car_y - sin(car_yaw));
+              ptsy.push_back(car_y);
+          } else {
+              // Redefine reference state as previous path end point
+              ref_x = previous_path_x[prev_size-1];
+              ref_y = previous_path_y[prev_size-1];
+              
+              double ref_x_prev = previous_path_x[prev_size-2];
+              double ref_y_prev = previous_path_y[prev_size-2];
+              
+              ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+              
+              // Use two points that make the path tangent to the previous path's end point
+              ptsx.push_back(previous_path_x[prev_size-2]);
+              ptsx.push_back(previous_path_x[prev_size-1]);
+              
+              ptsy.push_back(previous_path_y[prev_size-2]);
+              ptsy.push_back(previous_path_y[prev_size-1]);
+          }
+            
+          // In Frenet, add evenly 30m spaced points ahead of the starting reference
+          vector<double> next_wp0 = getXY(car_s+30, (2+4*0), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp1 = getXY(car_s+60, (2+4*0), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          vector<double> next_wp2 = getXY(car_s+90, (2+4*0), map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          
+          ptsx.push_back(next_wp0[0]);
+          ptsx.push_back(next_wp1[0]);
+          ptsx.push_back(next_wp2[0]);
 
-          json msgJson;
-
+          ptsy.push_back(next_wp0[1]);
+          ptsy.push_back(next_wp1[1]);
+          ptsy.push_back(next_wp2[1]);
+            
+          for (int i = 0; i < ptsx.size(); i++) {
+              // shift car reference angle to 0 degrees
+              double shift_x = ptsx[i] - ref_x;
+              double shift_y = ptsy[i] - ref_y;
+              
+              ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+              ptsy[i] = (shift_x * sin(0-ref_yaw) + shift_y * cos(0-ref_yaw));
+          }
+            
+          // create a spline
+          tk::spline s;
+            
+          // set (x,y) points to the spline
+          s.set_points(ptsx, ptsy);         
+            
+          // Define the actual (x,y) points we will use for the planner
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
@@ -99,17 +166,49 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+          
+          // Consider all possible state transition trajectories and select lowest cost
+          /*vector<double> xy = vehicle.chooseNextState(car_x, car_y, car_s, car_d, car_yaw, car_speed, 
+                                                      sensor_fusion, 
+                                                      map_waypoints_s, map_waypoints_x, map_waypoints_y)*/
+           
+          // Start with all of the previous path points from last time
+          for (int i = 0; i < previous_path_x.size(); i++) {
+              next_x_vals.push_back(previous_path_x[i]);
+              next_y_vals.push_back(previous_path_y[i]);
+          }
             
-          double dist_inc = 0.5;
-          for (int i = 0; i < 50; ++i) {
-            double next_s = car_s + (i+1) * dist_inc;
-            double next_d = 6;
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          // Calculate how to break up spline points so that we travel at our desired reference velocity
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x + target_y*target_y);
+          
+          double x_add_on = 0;
+            
+          // Fill up the rest of our path planner after filling it with previous points, here we will always output 50 points
+          for (int i = 1; i <= 50-previous_path_x.size(); i++) {
+              double N = target_dist / (.02 * ref_vel / 2.24);
+              double x_point = x_add_on + target_x / N;
+              double y_point = s(x_point);
               
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
+              x_add_on = x_point;
+              
+              double x_ref = x_point;
+              double y_ref = y_point;
+              
+              // rotate back to normal after rotating it earlier
+              x_point = x_ref * cos(ref_yaw) - y_ref * sin(ref_yaw);
+              y_point = x_ref * sin(ref_yaw) + y_ref * cos(ref_yaw);
+              
+              x_point += ref_x;
+              y_point += ref_y;
+              
+              next_x_vals.push_back(x_point);
+              next_y_vals.push_back(y_point);
           }
 
+          // Update car path
+          json msgJson;
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
